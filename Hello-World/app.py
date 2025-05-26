72f8dd869df3e9ef8b0d946186b6f27662439330
lea-f201 <leafata2002@gmail.com>
2025-05-26 10:54:19 +0300

    Update app.py

--- Hello-World/app.py
+++ Hello-World/app.py
@@ -143,7 +143,7 @@ with col[2]:
     average_education["Latitude"] = average_education["refArea"].map(lambda x: coords.get(x, (None, None))[0])
     average_education["Longitude"] = average_education["refArea"].map(lambda x: coords.get(x, (None, None))[1])
     average_education = average_education.dropna(subset=["Latitude", "Longitude"])
-
+    average_education = average_education[average_education["refArea"] != "Hasbaya District"]
     # Plot map
     map = px.scatter_mapbox(
         average_education,
